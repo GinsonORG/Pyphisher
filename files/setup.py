a062c3bee5611027e71d9ccaf8f5cff7f0e58822
Ginson <73432258+GinsonORG@users.noreply.github.com>
2023-04-26 08:35:11 +0530

    Update setup.py

--- files/setup.py
+++ files/setup.py
@@ -11,10 +11,10 @@ setup(
     description='Ultimate phishing tool in python with dual tunneling, 77 templates and many more!',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='KasRoudra',
-    author_email='kasroudrakrd@gmail.com',
+    author='GinsonORG',
+    author_email='gamergnsn@gmail.com',
     license="MIT",
-    url='https://github.com/KasRoudra/PyPhisher/',
+    url='https://github.com/GinsonORG/PyPhisher/',
     scripts=['pyphisher'],
     install_requires=["requests", "bs4", "rich"]
 )
